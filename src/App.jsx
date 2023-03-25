b92d81c1e0a3b49ef644412d4e1185a878e50f66
venutti <lvenutti@fi.uba.ar>
2023-03-25 17:44:08 -0300

    Une los componentes en app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CarForm from "./components/CarForm";
+import CarList from "./components/CarList";
+import CarSearch from "./components/CarSearch";
+import CarValue from "./components/CarValue";
+
+const App = () => {
+  return (
+    <div>
+      <CarForm />
+      <CarList />
+    </div>
+  );
+};
+
+export default App;
